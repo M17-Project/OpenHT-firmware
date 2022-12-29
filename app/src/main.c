5d8c72d3e53279702534fc9c34c801a7f26b54a4
Leo Hofer <leo.hofer@hofernet.ch>
2022-12-29 14:08:01 +0100

    blink

--- app/src/main.c
+++ app/src/main.c
@@ -3,11 +3,33 @@
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/spi.h>
+#include <zephyr/drivers/gpio.h>
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
 
+// devicetree node alias
+#define LED0_NODE DT_ALIAS(led0)
+
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
 void main(void)
 {
-  k_msleep(SLEEP_TIME_MS);
+  int ret;
+
+	if (!device_is_ready(led1.port)) {
+		return;
+	}
+
+  ret = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return;
+	}
+
+  while(1) 
+  {
+    gpio_pin_toggle_dt(&led1);
+
+    k_msleep(SLEEP_TIME_MS);
+  }
 }
